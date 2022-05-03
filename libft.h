6d7dc7152982c88e21695d5bea726cf17059005c
lfarias- <leofariasrj25@gmail.com>
2022-05-03 13:41:26 -0300

    Add libft and some initial functions

---
+++ libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/03 13:38:42 by lfarias-          #+#    #+#             */
+/*   Updated: 2022/05/03 13:41:03 by lfarias-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef
+# include <stdlib.h>
+# include <stddef.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isascii(int c);
+int	ft_isalnum(int c);
+#endif
