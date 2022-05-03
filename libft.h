5022f983daf5f8f6851eb8c6272997205b9e3ace
lfarias- <leofariasrj25@gmail.com>
2022-05-03 17:28:31 -0300

    Add ft_isprint(all green) and update libft.h with its prototype

--- libft.h
+++ libft.h
@@ -6,12 +6,12 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 13:38:42 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/03 13:51:17 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/03 17:27:54 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 # include <stdlib.h>
 # include <stddef.h>
 
@@ -19,4 +19,5 @@ int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isascii(int c);
 int	ft_isalnum(int c);
+int	ft_isprint(int c);
 #endif
