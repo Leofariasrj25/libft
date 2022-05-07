a0f43a58a8b2dddb139b0d294ff49497aaa509aa
lfarias- <leofariasrj25@gmail.com>
2022-05-07 15:37:10 -0300

    Fix ft_atoi to follow norminette rules

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 14:28:49 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/07 14:57:21 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/07 15:31:48 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 static int	ft_isspace(char c)
 {
-	if (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' ')
+	if (c == '\t' || c == '\n' || c == '\v' || c == '\f'
+		|| c == '\r' || c == ' ')
 		return (1);
 	else
 		return (0);
@@ -25,7 +26,7 @@ int	ft_atoi(const char *str)
 	int	res;
 	int	signal;
 	int	i;
-	
+
 	res = 0;
 	signal = 1;
 	i = 0;
@@ -34,10 +35,10 @@ int	ft_atoi(const char *str)
 	while (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
-			signal *=  -1;
+			signal *= -1;
 		i++;
 	}
-	while(ft_isdigit(str[i]))
+	while (ft_isdigit(str[i]))
 	{
 		res = res * 10 + (str[i] - 48);
 		i++;
