618ccd02e9e407de4c5d1db52c10d181392a088e
lfarias- <leofariasrj25@gmail.com>
2022-05-07 13:27:44 -0300

    Add ft_strlcat(all green) and update libft.h with its prototype

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,24 +6,27 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 19:40:58 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/05 19:57:05 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/07 13:24:03 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dstlen;
-	size_t	srclen;
 	size_t	i;
 
 	dstlen = ft_strlen(dst);
+	if (dstsize == 0)
+		return (dstlen + ft_strlen(src));
 	i = 0;
-	while (i < srclen && src[i] != '\0')
+	while (((dstlen + i) < (dstsize - 1)) && src[i] != '\0')
 	{
 		dst[dstlen + i] = src[i];
 		i++;
 	}
+	dst[dstlen + i] = '\0';
 	return (dstlen + ft_strlen(src));
 }
