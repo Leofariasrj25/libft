1511e1ec6a1e386c77e8ab704fd2e8f188b19244
lfarias- <leofariasrj25@gmail.com>
2022-05-09 18:28:28 -0300

    Fix ft_calloc to deal with zero size or zero pointer

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 15:01:01 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/07 15:09:26 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/09 18:22:46 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
+	if (count == 0 || size == 0)
+	{
+		count = 1;
+		size = 1;
+	}
 	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
