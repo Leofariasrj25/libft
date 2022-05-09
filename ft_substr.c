7341e46ccb3c02602de00c8efd11b7c66312d090
lfarias- <leofariasrj25@gmail.com>
2022-05-09 11:00:14 -0300

    fix incorrerct assumption about start and len args in ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 17:00:35 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/07 17:21:55 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/09 10:59:13 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 
-	if (start < len)
-	{
-		substr = ft_calloc(len + 1, sizeof(char));
-		if (!substr)
-			return (NULL);
-		ft_strlcpy(substr, &s[start], len + 1);
-		return (substr);
-	}
-	else
+	substr = ft_calloc(len + 1, sizeof(char));
+	if (!substr)
 		return (NULL);
+	ft_strlcpy(substr, &s[start], len + 1);
+	return (substr);
 }
