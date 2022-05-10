cbb2ef154f9139fe6bc8169ee2c7a70bf0ca4de1
lfarias- <leofariasrj25@gmail.com>
2022-05-10 16:31:41 -0300

    Add ft_strmapi(all green) and update libft.h with its prototype

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 13:38:42 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/09 19:23:57 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/10 16:15:57 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,5 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 #endif
