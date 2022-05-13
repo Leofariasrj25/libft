47ef9eed49032d53f7fc1ae7cef270cd41ce3b10
lfarias- <leofariasrj25@gmail.com>
2022-05-13 16:34:50 -0300

    Add ft_lstadd_back (all green)

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/13 16:07:39 by lfarias-          #+#    #+#             */
+/*   Updated: 2022/05/13 16:34:19 by lfarias-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last_node;
+
+	last_node = ft_lstlast(*lst);
+	last_node->next = new;
+	new->next = NULL;
+}
