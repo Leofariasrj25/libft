76be9c64f7a2aeb25c8ecb9b78047c5a9cd56822
lfarias- <leofariasrj25@gmail.com>
2022-05-14 17:11:56 -0300

    Refactor ft_lstadd_block to prioritize empty lists

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 16:07:39 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/13 16:44:54 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/14 16:52:27 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last_node;
 
-	if (lst && new)
+	
+	if (!lst && new)
+	{
+		*lst = new;
+		new->next = NULL;
+	}
+	else if (lst && new)
 	{
 		last_node = ft_lstlast(*lst);
 		last_node->next = new;
-		new->next = NULL;
-	}
-	else if (!lst && new)
-	{
-		*lst = new;
 		new->next = NULL;
 	}
 }
