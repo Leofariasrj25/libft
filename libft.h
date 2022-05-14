dfb52e0d4b8c78aa08b2300b2825c8e03afdcf45
lfarias- <leofariasrj25@gmail.com>
2022-05-14 12:17:02 -0300

    Add ft_lstdelone (all green) and update libft.h with its prototype

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 13:38:42 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/13 16:47:17 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/13 18:22:01 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,4 +62,5 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 #endif
