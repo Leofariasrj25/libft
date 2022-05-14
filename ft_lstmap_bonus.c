49e5bda79fda5690612045ea8f95f0fff4621590
lfarias- <leofariasrj25@gmail.com>
2022-05-14 17:18:46 -0300

    Add ft_lstmap (all green)

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 15:16:07 by lfarias-          #+#    #+#             */
+/*   Updated: 2022/05/14 17:17:58 by lfarias-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*new_node;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new_list = ft_lstnew((*f)(lst->content));
+	lst = lst->next;
+	while (lst)
+	{
+		new_node = ft_lstnew((*f)(lst->content));
+		if (!new_node)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
