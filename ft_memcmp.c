1e0a9c8dcd15f86d34dcb0049e8cbe441ad53677
Leonardo Farias <leofariasrj25@gmail.com>
2022-05-15 16:44:25 -0300

    Fix ft_memcmp to follow norminette rules

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,13 +6,9 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 12:12:23 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/05 12:56:50 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/15 13:38:18 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// o código assume que as duas strings possuem o mesmo tamanho n
-// the function should return 0 if they are equal or are of size 0
-// first the basic functioning then edge cases
 
 #include "libft.h"
 
@@ -25,9 +21,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	p1 = (unsigned char *) s1;
 	p2 = (unsigned char *) s2;
-	while (i < n && p1[i] == p2[i])
+	while (n && p1[i] == p2[i])
 	{
 		i++;
+		n--;
 	}
-	return ((unsigned char) p1[i] - (unsigned char) p2[i]);
+	if (n == 0)
+		return (0);
+	return (p1[i] - p2[i]);
 }
