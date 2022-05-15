2fb078a74ced4bec95a548059ce21fd21dfb1305
Leonardo Farias <leofariasrj25@gmail.com>
2022-05-15 15:49:49 -0300

    Refactor ft_strncmp to better deal with edge cases

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 14:22:52 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/15 14:15:36 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/15 14:44:22 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] != '\0' && i < n - 1)
+	while (s1[i] != '\0' && s2[i] != '\0' && n > 0)
 	{
-		if (s1[i] == s2[i])
-			i++;
-		else
+		if (s1[i] != s2[i])
 			break ;
+		n--;
+		i++;
 	}
+	if (n == 0)
+		return (n);
 	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 }
