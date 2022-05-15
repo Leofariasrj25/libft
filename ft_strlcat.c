ec37f9b2b5234796bbe97439d8222ebbda94b1f1
Leonardo Farias <leofariasrj25@gmail.com>
2022-05-15 20:10:17 -0300

    Fix ft_strlcat with data from unit tests

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,27 +6,64 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 19:40:58 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/07 13:24:03 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/15 20:08:05 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+#include <bsd/string.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dstlen;
+	size_t	n;
 	size_t	i;
 
-	dstlen = ft_strlen(dst);
-	if (dstsize == 0)
-		return (dstlen + ft_strlen(src));
+	n = dstsize;
 	i = 0;
-	while (((dstlen + i) < (dstsize - 1)) && src[i] != '\0')
+	while (n != 0 && dst[i] != '\0')
 	{
-		dst[dstlen + i] = src[i];
 		i++;
+		n--;
+	}
+	dstlen = i;
+	if (n == 0)
+		return (dstlen + ft_strlen(src));
+	i = -1;
+	while (src[++i] != '\0')
+	{
+		if (n > 1)
+		{
+			dst[dstlen + i] = src[i];
+			n--;
+		}
 	}
 	dst[dstlen + i] = '\0';
-	return (dstlen + ft_strlen(src));
+	return (dstlen + i);
 }
+
+/*int main(void)
+{
+	
+	char s1[4] = "";
+	char s2[4] = "";
+	int r1;
+	int r2;
+	r1 = strlcat(s1, "thx to ntoniolo for this test !", 4);
+	r2 = ft_strlcat(s2, "thx to ntoniolo for this test !", 4);
+	
+	printf("%d -> %s\n", r1, s1);
+	printf("%d -> %s\n", r2, s2);
+
+	char *str = "the cake is a lie !\0I'm hidden lol\r\n";
+	char buff1[3840] = "there is no stars in the sky";
+	char buff2[3840] = "there is no stars in the sky";
+	size_t max = strlen("the cake is a lie !\0I'm hidden lol\r\n") + 4;
+	printf("max %lu\n", max);
+	r1 = strlcat(buff1, str, max);
+	r2 = ft_strlcat(buff2, str, max);
+
+	printf("r1 %d -> %s\n", r1, buff1);
+	printf("r2 %d -> %s\n", r2, buff2);
+}*/
