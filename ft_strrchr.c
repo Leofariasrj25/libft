82c5761b89612f39fbcac8dc80b977f4e3b74307
Leonardo Farias <leofariasrj25@gmail.com>
2022-05-15 13:15:36 -0300

    Fix ft_strrchr.c based on unit tests

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 15:43:41 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/05 16:11:57 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/15 13:13:50 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t	s_len;
+	int		s_len;
 	char	find;
 
 	s_len = ft_strlen(s);
 	find = (char) c;
-	while (s[s_len] >= '\0')
+	while (s_len >= 0)
 	{
 		if (s[s_len] == find)
 			return ((char *) &s[s_len]);
