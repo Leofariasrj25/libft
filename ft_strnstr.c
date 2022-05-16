44110a1a9fbfaab3cfe996d55be17ca24a33a8d6
Leonardo Farias <leofariasrj25@gmail.com>
2022-05-15 22:01:03 -0300

    Fix ft_strnstr with feedback from unit tests

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 13:29:48 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/07 13:56:20 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/05/15 21:59:47 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	char	*str;
 	size_t	i;
 	size_t	needle_len;
 
 	i = 0;
 	needle_len = ft_strlen(needle);
-	if (*needle == '\0')
-		return ((char *) haystack);
-	while (i < len)
+	str = (char *) haystack;
+	while (haystack[i] != '\0')
 	{
-		if (ft_strncmp(&haystack[i], needle, needle_len - 1) == 0)
-		{
-			return ((char *) &haystack[i]);
-		}
+		if (ft_strncmp(str, needle, needle_len) == 0
+			&& needle_len + i <= len)
+			return (str);
+		str++;
 		i++;
 	}
 	return (NULL);
