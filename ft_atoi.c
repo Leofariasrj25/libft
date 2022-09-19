a55f44eb7b7863f72b19ba2d33d51a0436520496
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2022-09-19 20:42:49 -0300

    update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,20 +6,11 @@
 /*   By: lfarias- <leofariasrj25@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 14:28:49 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/05/10 16:12:19 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/09/19 20:30:09 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_isspace(char c)
-{
-	if (c == '\t' || c == '\n' || c == '\v' || c == '\f'
-		|| c == '\r' || c == ' ')
-		return (1);
-	else
-		return (0);
-}
 
 int	ft_atoi(const char *str)
 {
