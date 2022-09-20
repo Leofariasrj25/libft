b435189a7fe0eb4f376e86a132dc5604f94ad356
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2022-09-19 22:43:02 -0300

    update some files

--- ft_islower.c
+++ ft_islower.c
@@ -6,13 +6,13 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 15:38:34 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/09/19 16:55:24 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/09/19 17:30:43 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_islower(int c)
 {
-	char letter;
+	char	letter;
 
 	letter = (char) c;
 	if (letter >= 97 && letter <= 122)
